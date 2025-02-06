0deea629d2315ed7c1b824cfcb33162defb3e688
zagalseb <zagalseb@gmail.com>
2025-02-06 08:36:41 -0600

    Add files via upload

--- rutinas.js
+++ rutinas.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonContainer = document.getElementById("nav-button-container");
+
+    // Recuperar el rol desde localStorage
+    const userRole = localStorage.getItem("role");
+
+    if (userRole === "MAYOR" || userRole === "COACHES") {
+        const button = document.createElement("a");
+        button.className = "centered-button"; // Aplica la clase corregida
+        button.href = "pruebas.html";
+        button.textContent = "Ir a Pruebas";
+        buttonContainer.appendChild(button);
+    }
+});
+
+
+// Cargar las rutinas desde el archivo TSV
 async function loadRoutineNames() {
     const response = await fetch('Rutinas.tsv');
     const tsvText = await response.text();
@@ -13,10 +30,13 @@ async function loadRoutineNames() {
         const link = document.createElement('a');
         link.href = `detallerutina.html?routine=${index}`;
         link.textContent = row[0]; // Nombre de la rutina
+        link.className = "button-link"; // Aplicar el mismo estilo que otros botones
         listItem.appendChild(link);
         list.appendChild(listItem);
     });
 }
 
 loadRoutineNames();
+
+
 
